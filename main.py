6ee26c35e0ef9106dbd6c7dfad18d2537b3a6615
Nick Lohmann <nicklohmann@outlook.de>
2024-04-08 13:56:30 +0200

    Segment Display should be done, now. - Dual button cycles in different directions between Time, Temperature and Humidity

--- main.py
+++ main.py
@@ -3,11 +3,49 @@ HOST = "172.20.10.242"
 PORT = 4223
 
 from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_dual_button_v2 import BrickletDualButtonV2
 import mod_humidity as hum
 import mod_temperature as temp
 import mod_dualButton as dButton
 import mod_rgbButton as rgbButton
 import mod_segDisplay as segDisp
+
+ipcon = ""
+
+def callback_DualButton(button_l, button_r, led_l, led_r):
+    if button_l == BrickletDualButtonV2.BUTTON_STATE_PRESSED:
+        print("Left Button: Pressed")
+
+        #cycle T > H > C
+        if segDisp.mode == "T":
+            segDisp.mode = "H"
+        elif segDisp.mode == "H":
+            segDisp.mode = "C"
+        else:
+            segDisp.mode = "T"
+
+        print("Cycled to '" + segDisp.mode + "'. Updating")
+        if ipcon != "":
+            segDisp.UpdateDisplay(ipcon, False)
+    elif button_l == BrickletDualButtonV2.BUTTON_STATE_RELEASED:
+        print("Left Button: Released")
+
+    if button_r == BrickletDualButtonV2.BUTTON_STATE_PRESSED:
+        print("Right Button: Pressed")
+
+        #cycle C > H > T
+        if segDisp.mode == "C":
+            segDisp.mode = "H"
+        elif segDisp.mode == "H":
+            segDisp.mode = "T"
+        else:
+            segDisp.mode = "C"
+
+        print("Cycled to '" + segDisp.mode + "'. Updating")
+        if ipcon != "":
+            segDisp.UpdateDisplay(ipcon, False)
+    elif button_r == BrickletDualButtonV2.BUTTON_STATE_RELEASED:
+        print("Right Button: Released")
 
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection
@@ -20,13 +58,8 @@ if __name__ == "__main__":
     try:
 
         temp.printTemperature(ipcon)
-
-        
         hum.printHumidity(ipcon)
-
-        segDisp.setHumidity(ipcon)
-        segDisp.setTemperature(ipcon)
-
+        dButton.bind(ipcon, callback_DualButton)
 
 
     except Exception as e:
@@ -37,5 +70,8 @@ if __name__ == "__main__":
 
     input("Press key to exit\n") # Use raw_input() in Python 2
 
+    dButton.unbind(ipcon)
     ipcon.disconnect()
+    
+
 
--- mod_dualButton.py
+++ mod_dualButton.py
@@ -1,11 +1,15 @@
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_dual_button_v2 import BrickletDualButtonV2
 
-def dbutton_bind(ipcon, nr, callback):
-    db = BrickletDualButtonV2("23Qx", ipcon)
+def bind(ipcon, callback):
+    db = BrickletDualButtonV2("Vd8", ipcon)
 
     db.register_callback(db.CALLBACK_STATE_CHANGED, callback)
     
     db.set_state_changed_callback_configuration(True)
 
     print("Bound Button to Function")
+
+def unbind(ipcon):
+    db = BrickletDualButtonV2("Vd8", ipcon)
+    db.reset()
